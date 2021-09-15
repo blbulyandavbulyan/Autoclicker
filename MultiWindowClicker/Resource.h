aa47b0a36e7e6de55619982d15e741e10075d922
Давид Блбулян <blbulyandavbulyan@gmail.com>
2021-09-15 23:20:10 +0300

    Created  New Project

---
+++ MultiWindowClicker/Resource.h
@@ -0,0 +1,43 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется MultiWindowClicker.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MULTIWINDOWCLICKER_DIALOG   102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MULTIWINDOWCLICKER          107
+#define IDI_SMALL                       108
+#define IDC_MULTIWINDOWCLICKER          109
+#define IDR_MAINFRAME                   128
+#define IDD_MAIN                        129
+#define IDD_DIALOG2                     130
+#define IDD_DIALOG_ADD_NEW_WINDOW       130
+#define IDC_LIST_ADDED_WINDOWS          1000
+#define IDC_RADIO1                      1001
+#define IDC_RADIO2                      1002
+#define IDC_NAME_FOR_SEARCH_WINDOW      1004
+#define IDC_SPIN_X                      1006
+#define IDC_EDIT_X                      1007
+#define IDC_EDIT_Y                      1008
+#define IDC_SPIN_Y                      1009
+#define IDC_ADD_NEW_WINDOW              1010
+#define IDC_SLIDER2                     1012
+#define IDC_CHECK2                      1013
+#define IDC_LIST1                       1014
+#define IDC_COMBO1                      1015
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
